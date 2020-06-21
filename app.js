f5fb427d7970e8fc01e0135e4f96c21a5e16a2e6
adrianoduque97 <adrian.duque@capmation.com>
2020-06-21 12:44:54 -0500

    V2 Changes - Added more REST methods - Update the add new Camp - New view for more details

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const bodyParser = require('body-parser')
 
+mongoose.connect("mongodb://localhost/yelp_camp",{ useNewUrlParser: true, useUnifiedTopology: true })
 app.use(bodyParser.urlencoded({extended: true}))
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-];
-
 app.set('view engine','ejs')
+
+// SCHEMA
+
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+})
+const Campground = mongoose.model('Campground',campgroundSchema)
+// Campground.create(
+//     {name: "Salmon Creek", 
+//     image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg",
+//     description: "Salmon Creek Description camp, no bathroom"})
+//                 .then(camp => console.log(camp))
+//                 .catch(err=>console.log(err))
+
+// var campgrounds = [
+//     {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+//     {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+//     {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+//     {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+//     {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+//     {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+//     {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+//     {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+//     {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+// ];
+
 
 app.get("/", (req,res)=>{
     res.render("landing")
@@ -23,7 +41,13 @@ app.get("/", (req,res)=>{
 
 app.get("/campgrounds", (req,res)=>{
 
-    res.render('campgrounds',{campgrounds: campgrounds})
+    Campground.find()
+                .then((camp)=>{
+                    res.render('index',{campgrounds: camp})
+                })
+                .catch(err => console.log(err))
+
+    //res.render('campgrounds',{campgrounds: campgrounds})
 
 })
 
@@ -36,8 +60,26 @@ app.get("/campgrounds/new", (req,res)=>{
 app.post("/campgrounds",(req,res)=>{
     const name =req.body.name
     const image = req.body.image
-    campgrounds.push({name: name, image:image})
+    const descr = req.body.description
+
+    const camp = new Campground({
+        name:name,
+        image:image,
+        description:descr
+    })
+    camp.save()
+        .then(camp=> console.log(`New Campgroun added ${camp}`))
+        .catch(err => console.log(err))
     res.redirect("/campgrounds")
+})
+
+app.get("/campgrounds/:id",(req,res)=>{
+    Campground.findById(req.params.id)
+            .then(camp => res.render("show",{campground: camp}))
+            .catch(err=>console.log(err))   
+    
+   // Campground.find()
+
 })
 
 
